27be89715f6dff0adb97a16ec3dcb1f5a42ad5c8
Anthony Guamaninga <anthonysub300@gmail.com>
2024-01-24 20:55:14 -0500

    Taller 26: Implementación de API con Spring Boot

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -1,5 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repository.modelo.Estudiante;
+
+/*
+ * Una API puede tener mucho servicios
+ * API: por el proyecto 
+ */
+
+//Servicio -> Controller: clase controller 
+@RestController //Servicio
 public class EstudianteControllerRestFul {
 
+	//Metodos: capacidades
+	public void guardar(Estudiante estudiante) {
+		
+	}
 }
