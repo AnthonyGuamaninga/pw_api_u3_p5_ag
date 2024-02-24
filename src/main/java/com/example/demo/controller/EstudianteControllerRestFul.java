7b3f81114f388bf2560281d553919b3e29e12272
Anthony Guamaninga <anthonysub300@gmail.com>
2024-02-24 10:59:44 -0500

    Taller41: Security en el API Server

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -31,6 +32,8 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController // Servicio
 @RequestMapping(path = "/estudiantes")
+@CrossOrigin
+// (value = "http://localhost:8080") 
 public class EstudianteControllerRestFul {
 
 	@Autowired
